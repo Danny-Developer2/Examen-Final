c38cb7b4259aa50b3d6b0d4f010528934083230f
Danny-Developer2 <daniel_luevanolr@hotmail.com>
2024-11-22 17:02:17 -0600

    Se soluciona error del mensaje de Lista vacia

--- Prueba_T_Casoftt-main/API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ Prueba_T_Casoftt-main/API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d3ae2274f0d3958511702fad153ed28235e9e644")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+62f8bf99738582cbb1aca997fae592eac4040798")]
 [assembly: System.Reflection.AssemblyProductAttribute("Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
